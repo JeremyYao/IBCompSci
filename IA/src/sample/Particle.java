1a125d883edd448cac50dc0a4db4e82f2eed26a3
Jeremy <Jeremy.yao.info@gmail.com>
2019-01-25 12:19:26 -0500

    1/24 Create Particle class

--- IA/src/sample/Particle.java
+++ IA/src/sample/Particle.java
@@ -1,4 +1,19 @@
 package sample;
 
-public class Particle {
+public class Particle
+{
+    private double mass, velocityX, velocityY, accelX, accelY;
+    private static final double universalGravConst = 6.67408 * Math.pow(10,-11);
+
+    public Particle(double mass, double velocityx, double velocityy)
+    {
+        this.mass = mass;
+        this.velocityX = velocityx;
+        this.velocityY = velocityy;
+    }
+
+    public void updateAccel()
+    {
+
+    }
 }
